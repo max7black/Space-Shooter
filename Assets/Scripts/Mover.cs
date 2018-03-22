1e46b720e2abca18f5d5d00a7895c6e94662ffea
Cole Arnold <max7black@gmail.com>
2018-03-22 10:58:01 -0500

    Intial Commit
    Base game already created from tutorial on Unity. Added different
    background and coins that drop from asteroids.

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float speed;
+
+    void Start()
+    {
+        rb.velocity = transform.forward*speed;
+    }
+}
